8c2bca8838dab7a6d5c2e7deaadb5d3aa864cb34
桑榆肖物 <sang93@qq.com>
2025-03-09 23:50:25 +0800

    feat: 更新 Worker 类以支持音频数据处理和 WebSocket 消息解析

--- src/Worker.cs
+++ src/Worker.cs
@@ -13,30 +13,88 @@ public class Worker : IHostedService
 
     private readonly SessionUpdateOptions _sessionUpdateOptions;
 
+    private string nowUserMessage = "";
+
+    private string nowAIMessage = "";
+
+    // 是否已经丢弃首包的WAV音频头
+    private bool hasDiscardedWavHeader = false;
+
     public Worker(AudioService audioService, WebSocketService webSocketService, ILogger<Worker> logger, IOptions<SessionUpdateOptions> sessionUpdateOptions)
     {
-        _sessionUpdateOptions = sessionUpdateOptions.Value;
-        _audioService = audioService;
-        _webSocketService = webSocketService;
         _logger = logger;
+        _sessionUpdateOptions = sessionUpdateOptions.Value;
+        
+        _webSocketService = webSocketService;
         _webSocketService.OnMessageReceived += HandleWebSocketMessage;
         _webSocketService.OnConnected += HandleWebSocketConnected;
+
+        _audioService = audioService;
+        _audioService.OnAudioDataAvailable += HandleAudioDataAvailable;
+        _audioService.OnRecordingStopped += () =>
+        {
+            hasDiscardedWavHeader = false;
+        };
+    }
+
+    private async Task HandleAudioDataAvailable(byte[] obj)
+    {
+        if (!hasDiscardedWavHeader)
+        {
+            // 丢弃首包的WAV音频头
+            hasDiscardedWavHeader = true;
+            return;
+        }
+        // 处理接收到的音频数据  转为 base64
+        var message = new BaseMessage
+        {
+            Type = "input_audio_buffer.append",
+            Audio = Convert.ToBase64String(obj)
+        }.ToJson();
+        await _webSocketService.SendAsync(message);
     }
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         await _webSocketService.ConnectAsync();
+        await _audioService.StartRecordingAsync();
 
-        _audioService.RecordAudio("output.wav", 10);
-        _audioService.PlayAudio("output.wav");
+        //_audioService.RecordAudio("output.wav", 10);
+        //_audioService.PlayAudio("output.wav");
 
-        await _webSocketService.SendAsync("Audio recording and playback completed.");
+        //await _webSocketService.SendAsync("Audio recording and playback completed.");
     }
 
     private async Task HandleWebSocketMessage(string message)
     {
-        // 处理接收到的 WebSocket 消息
-        Console.WriteLine($"Handling message: {message}");
+        // 处理接收到的 WebSocket 消息 解析为 BaseMessage
+        var baseMessage = JsonSerializer.Deserialize<BaseMessage>(message);
+        if (baseMessage == null)
+        {
+            _logger.LogWarning("Received message is not a valid BaseMessage.");
+            return;
+        }
+        switch (baseMessage.Type)
+        {
+            case "conversation.item.input_audio_transcription.completed":
+                // 用户音频转文本完成
+                _logger.LogInformation($"User: {baseMessage.Transcript}");
+                nowUserMessage = baseMessage.Transcript;
+                break;
+            case "response.content_part.added":
+                // AI 准备文本回复
+                nowAIMessage = "";
+                _logger.LogInformation("AI: ");
+                break;
+            case "response.audio_transcript.delta":
+                // AI 文本回复
+                nowAIMessage += baseMessage.Delta;
+                _logger.LogInformation(baseMessage.Delta);
+                break;
+            default:
+                _logger.LogWarning($"Received message with unknown type: {baseMessage.Type}");
+                break;
+        }
     }
 
     private async Task HandleWebSocketConnected()
@@ -52,6 +110,7 @@ public class Worker : IHostedService
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
+        _audioService.StopRecording();
         return _webSocketService.CloseAsync();
     }
 }
