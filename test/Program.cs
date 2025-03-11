b7f82fbcdff5aac1b2e3a5d7eaa7752c73ded2a3
桑榆肖物 <sang93@qq.com>
2025-03-11 23:29:13 +0800

    feat: 添加音频录制和播放功能的测试程序

---
+++ test/Program.cs
@@ -0,0 +1,19 @@
+﻿using Alsa.Net;
+
+var settings = new SoundDeviceSettings
+{
+    MixerDeviceName = "hw:0",       // 混音设备
+    PlaybackDeviceName = "hw:0",     // 播放设备
+    RecordingDeviceName = "hw:0",    // 录音设备
+    RecordingSampleRate = 16_000     // 16kHz采样率
+};
+
+using var alsaDevice = AlsaDeviceBuilder.Create(settings);
+
+// 录制10秒音频
+Console.WriteLine("开始录音...");
+alsaDevice.Record(10, "output.wav");
+
+// 播放录制的音频
+Console.WriteLine("播放音频...");
+alsaDevice.Play("output.wav");
